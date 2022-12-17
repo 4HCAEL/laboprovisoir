1f140f347ecb2ce85f9d7aeeb13d378ca43e03b6
Pikachu <58916687+4HCAEL@users.noreply.github.com>
2022-12-17 21:31:20 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,19 +1,17 @@
 import socket
 
 hote = "192.168.1.131"
-port = 8000
+port = 8005
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((hote, port))
 
 while True:
-        print ("{} connected".format( address ))
-
-        response = client.recv(255)
+        response = socket.recv(255)
+        print (response.decode())
         inp=input("msg: ")
-        client.send(inp.encode())
+        socket.send(inp.encode())
         
-        if response != "":
-                print (response)
-print ("Close")
+
+print("Close")
 socket.close()
