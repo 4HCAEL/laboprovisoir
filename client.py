e4060d2c44c0dfa7ee490bdee713a2fa98833075
Pikachu <58916687+4HCAEL@users.noreply.github.com>
2022-12-17 20:33:29 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,13 @@
-import socket, multiprocessing
+import socket
 
-host = 'localhost'
+hote = "192.168.1.131"
 port = 8000
-nb_workers = 10
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((host, port))
-    s.listen(1)
-    with multiprocessing.Pool(nb_workers) as pool:
-        while True:
-            conn, address = s.accept()
-            pool.apply(handle, (conn, address))
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((hote, port))
+print "Connection on {}".format(port)
 
-def handle(conn, address):
-    with conn:
-        buff = conn.recv(512)
-        message = buff.decode('utf-8')
-        conn.sendall(f"echo : {message}".encode('utf-8'))
+socket.send("Hey my name is Olivier!")
+
+print "Close"
+socket.close()
